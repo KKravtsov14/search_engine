b3e5d86ce3e28cdd6f4b3a770da40a27a26d2a68
KKravtsov14 <kostya.kravtsov.2001@mail.ru>
2020-02-25 16:28:34 +0700

    called functions

--- main.py
+++ main.py
@@ -2,3 +2,33 @@
 # Kravtsov - %
 
 def main():
+
+    pass
+
+def acceptCommand():
+
+    pass
+
+def runCommand(command):
+
+    pass
+
+def moveUp():
+
+    pass
+
+def moveDown(currentDir):
+
+    pass
+
+def countFiles(path):
+
+    pass
+
+def countBytes(path):
+
+    pass
+
+def findFiles(target, path):
+
+    pass
