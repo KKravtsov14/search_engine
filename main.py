e4f27d4140c5e78976a55ad9c8a69c94a8695e84
Andrey <andreiliza1981@mail.ru>
2020-03-07 11:29:35 +0700

    Added main

--- main.py
+++ main.py
@@ -3,8 +3,22 @@
 import os
 
 def main():
-
-    pass
+    while True:
+        print(
+f'''Текущая директория: {os.getcwd()}
+1. Просмотр каталога
+2. На уровень вверх
+3. На уровень вниз
+4. Количество файлов и каталогов
+5. Размер текущего каталога (в байтах)
+6. Поиск файла
+7. Выход из программы''')
+        n_command = input('Введите пункт меню:')
+        command = acceptCommand(n_command)
+        runCommand(command)
+        if command == 7:
+            print('Работа программы завершена')
+            break
 
 def acceptCommand(n_command):
     lst = ['1', '2', '3', '4', '5', '6', '7']
@@ -14,8 +28,21 @@ def acceptCommand(n_command):
 
 
 def runCommand(command):
-
-    pass
+    if command == 2:
+        return moveUp()
+    if command == 3:
+        currentDir = input()
+        return moveDown(currentDir)
+    if command == 4:
+        path = str(input())
+        print(countFiles(path))
+    if command == 5:
+        path = str(input())
+        print(countBytes(path))
+    if command == 6:
+        path = str(input())
+        target = str(input()) #
+        print(findFiles(target, path))
 
 def moveUp():
     os.chdir('../')
@@ -60,3 +87,4 @@ def findFiles(target, path):
             elif target.lower() in i.lower():
                 k.append(path + '/' + i)
     return k
+main()
